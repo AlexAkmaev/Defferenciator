80c34e65a7cb57ca46b860a7985fd7d020999e5f
Алексей Акмаев <AlexAkmaev>
2020-05-15 00:46:38 +0300

    for den

--- node.cpp
+++ node.cpp
@@ -1,9 +1,25 @@
 #include "node.h"
 
 /*** class Variable ***/
-Variable::Variable(int x) : _x(x) {}
+Variable::Variable(std::string x) : _x(x) {}
 
-int Variable::Evaluate() const { return _x; }
+std::string Variable::Evaluate() const {
+  if (_x == "cos") {
+    return "-sin";
+  } else if (_x == "sin") {
+    return "cos";
+  } else if (_x == "ln") {
+    return "1/";
+  } else if (_x == "exp") {
+    return "exp";
+  } else if (_x == "x") {
+    return "x";
+  } 
+}
+
+std::string Variable::self() const {
+	return Evaluate();
+}
 
 /*** class Op ***/
 Op::Op(std::string val)
@@ -12,45 +28,24 @@ Op::Op(std::string val)
           return 6;
         } else if (val == "+" || val == "-") {
           return 5;
-        } else if (val == "<" || val == "<=" || val == ">" || val == ">=") {
-          return 4;
-        } else if (val == "!=" || val == "==") {
-          return 3;
-        } else if (val == "&&") {
-          return 2;
-        } else {
-          return 1;
-        }
       }()),
       _op(val) {}
 
-int Op::Evaluate() const {
+std::string Op::Evaluate() const {
   if (_op == "*") {
-    return _left->Evaluate() * _right->Evaluate();
+    return _left->Evaluate() + "*" + _right->self + "+" + _left->self + "*" + _right->Evaluate();
   } else if (_op == "+") {
-    return _left->Evaluate() + _right->Evaluate();
+    return _left->Evaluate() + "+" + _right->Evaluate();
   } else if (_op == "-") {
-    return _left->Evaluate() - _right->Evaluate();
+    return _left->Evaluate() + "-" + _right->Evaluate();
   } else if (_op == "/") {
-    return _left->Evaluate() / _right->Evaluate();
-  } else if (_op == "<") {
-    return (_left->Evaluate() < _right->Evaluate());
-  } else if (_op == "<=") {
-    return (_left->Evaluate() <= _right->Evaluate());
-  } else if (_op == ">") {
-    return (_left->Evaluate() > _right->Evaluate());
-  } else if (_op == ">=") {
-    return (_left->Evaluate() >= _right->Evaluate());
-  } else if (_op == "==") {
-    return (_left->Evaluate() == _right->Evaluate());
-  } else if (_op == "!=") {
-    return (_left->Evaluate() != _right->Evaluate());
-  } else if (_op == "&&") {
-    return (_left->Evaluate() && _right->Evaluate());
-  } else if (_op == "||") {
-    return (_left->Evaluate() || _right->Evaluate());
-  }
+    return "(" + _left->Evaluate() + "*" + _right->self + "+" + _left->self + "*" + _right->Evaluate() +
+		                 ")/(" + _right->Evaluate() + ")^2";
   return 0;
+}
+
+std::string Variable::self() const {
+	return "|OP|";
 }
 
 void Op::SetLeft(std::shared_ptr<Node> node) { _left = node; }
